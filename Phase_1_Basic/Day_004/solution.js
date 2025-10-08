f6593f1ef9616a2366a3242b4918af9410823124
Raul Lize Teixeira <lizeraul1@gmail.com>
2025-10-08 13:35:36 -0300

    feat: implement contarFrequencia function to count name occurrences in an array

--- Phase_1_Basic/Day_004/solution.js
+++ Phase_1_Basic/Day_004/solution.js
@@ -1,5 +1,8 @@
 function contarFrequencia(arrayDeNomes) {
-    
+    return arrayDeNomes.reduce((acc, nomeAtual) => {
+        acc[nomeAtual] = acc[nomeAtual] ? acc[nomeAtual] + 1 : 1
+        return acc
+    }, {})
 }
 
 arr = ['João', 'Maria', 'João', 'Ana', 'Maria', 'João']
