01062ed5568cad33dcd7ba732a927168601ad104
Raul Lize Teixeira <lizeraul1@gmail.com>
2025-10-21 19:09:18 -0300

    feat: implement binary search function skeleton
    Add initial structure for binary search function with basic parameters and test case. The function will search for a target value in a sorted array.

---
+++ Phase_1_Basic/Day_009/solution.js
@@ -0,0 +1,9 @@
+function buscaBinaria(arrayOrdenado, alvo) {
+    let inicio = 0
+    let fim = arrayOrdenado.length - 1
+    // let meio = 
+}
+
+arr = [2, 5, 8, 10, 15, 20]
+
+console.log(buscaBinaria(arr, 15))
