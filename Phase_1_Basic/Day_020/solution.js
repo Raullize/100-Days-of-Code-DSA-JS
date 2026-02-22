38db2181bedc02c889d4b771d7988e68ecd0a453
Raul Lize Teixeira <lizeraul1@gmail.com>
2026-02-22 00:19:00 -0300

    feat: implement rotate function to rotate an array by k steps

--- Phase_1_Basic/Day_020/solution.js
+++ Phase_1_Basic/Day_020/solution.js
@@ -1,5 +1,22 @@
 function rotate(nums, k) {
+    k = k % nums.length
 
+    function reverse(arr, inicio, fim) {
+        while(inicio < fim) {
+            let temp = arr[inicio]
+            arr[inicio] = arr[fim]
+            arr[fim] = temp
+            
+            inicio++
+            fim--
+        }
+    }
+
+    reverse(nums, 0, nums.length - 1)
+    reverse(nums, 0, k - 1)
+    reverse(nums, k, nums.length - 1)
+
+    return nums
 }
 
 const arr = [1, 2, 3, 4, 5, 6, 7]
